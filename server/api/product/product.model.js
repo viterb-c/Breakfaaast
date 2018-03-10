10be8698dfbe7449068088c36046ef78ffe31a23
Charles Viterbo <charlesviterbo@gmail.com>
2018-03-10 19:37:20 +0000

    Model product
    Modify product's model  : 
    + name
    + description
    + price
    + picture

--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -4,9 +4,19 @@ import mongoose from 'mongoose';
 import {registerEvents} from './product.events';
 
 var ProductSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true
+  },
+  picture: {
+    type: String,
+    default: null
+  }
 });
 
 registerEvents(ProductSchema);
